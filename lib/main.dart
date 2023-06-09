851a85168b7ceeaa075b7243d8db950d38c09475
Daniramadani <daniramadani420@gmail.com>
2023-06-09 21:33:07 +0800

    melakukan perubahan pada main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,52 +2,85 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-class _MyAppState extends State<MyApp> {
-  List<dynamic> todos = [];
-
-  @override
-  void initState() {
-    super.initState();
-    fetchData();
-  }
-
-  Future<void> fetchData() async {
-    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos/'));
+  Future<List<dynamic>> getData() async {
+    var url = Uri.parse('https://jsonplaceholder.typicode.com/todos/');
+    var response = await http.get(url);
     if (response.statusCode == 200) {
-      setState(() {
-        todos = json.decode(response.body);
-      });
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to fetch data');
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Data JSON',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Data JSON'),
+          title: const Text("Tugas Integrasi Data"),
         ),
-        body: ListView.builder(
-          itemCount: todos.length,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              title: Text(todos[index]['title']),
-              subtitle: Text(todos[index]['completed'].toString()),
-            );
-          },
+        body: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: FutureBuilder<List<dynamic>>(
+            future: getData(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasData) {
+                List<dynamic> todos = snapshot.data!;
+                return ListView.builder(
+                  itemCount: todos.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      elevation: 4,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundColor: Colors.blue,
+                          child: Text(
+                            todos[index]['id'].toString(),
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        title: Text(
+                          todos[index]['title'],
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Text(
+                          todos[index]['completed']
+                              ? "Completed"
+                              : "Not Completed",
+                        ),
+                        trailing: Icon(
+                          todos[index]['completed']
+                              ? Icons.check_circle
+                              : Icons.cancel,
+                          color: todos[index]['completed']
+                              ? Colors.green
+                              : Colors.red,
+                        ),
+                      ),
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text('Failed to fetch data'),
+                );
+              }
+              return Container(); // Placeholder jika tidak ada data atau error
+            },
+          ),
         ),
       ),
     );
